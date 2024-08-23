73c25d78087367798deb1e375a788f18114f1de5
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-08-23 10:37:07 -0300

    Update api.js

--- src/componentes/services/api.js
+++ src/componentes/services/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: 'http://10.67.23.125:3333' // desenvolvimento
-    //baseURL: 'https://www.onlinecloze.com.br/apionlinecloze' // produção onlinecloze.com.br:21242
-});
+   });
 
 export default api;
